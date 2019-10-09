d301075487bfe9035331e5b0dc7316b1d0c20f6e
mortalvegetable <56037608+mortalvegetable@users.noreply.github.com>
2019-10-09 15:01:07 -0400

    Update Question1.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,17 @@
-# modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
-    # modify and then return the variable below
-    answer = -5
-    return answer
+    interestPercentage /= 100
+    repaymentPercentage /= 100
+    totalCost = .1 * initialLevelOfDebt
+    initialLevelOfDebt -= totalCost
+    repaymentCost = repaymentPercentage * initialLevelOfDebt
+    if repaymentCost < 50:
+        repaymentCost = 50
+    while initialLevelOfDebt > 0:
+        totalDebt = initialLevelOfDebt + (initialLevelOfDebt * interestPercentage)
+        totalCost += repaymentCost
+        totalDebt -= repaymentCost
+        print("total cost is: ", totalCost)
+        initialLevelOfDebt = totalDebt
+        print("initial debt is ", initialLevelOfDebt)
+    totalCost += repaymentCost
+    return totalCost
